61aafdc579b8253fc1a6dd94042e3259e9087d07
Dio-zhu <zzjbarry@126.com>
2019-08-24 15:32:29 +0800

    补充说明

--- src/MetaType.js
+++ src/MetaType.js
@@ -2,39 +2,30 @@
  * 适配页面元数据的输入类型定义
  */
 module.exports = {
-    /** css 类型-已过期begin */
-    // Background: 'Background',
-    // Border: 'Border',
-    // Radius: 'Radius',
-    // Font: 'Font',
-    // Margin: 'Margin',
-    // Size: 'Size',
-    // Padding: 'Padding',
-    /***css 类型-已过期end***/
     /** 基本类型 */
-    Bool: 'Bool',
-    Checkbox: 'Checkbox',
-    Color: 'Color',
-    Select: 'Select',
-    Text: 'Text',
-    Icon: 'Icon',
-    Number: 'Number',
-    Date:'Date',
-    Time:'Time',
-    Refer:'Refer',
-    SelectTree:'SelectTree',
-    Textarea:'Textarea',
+    Select: 'Select',//下拉选择
+    Text: 'Text',//文本输入
+    Bool: 'Bool',//开关
+    Number: 'Number',//数值输入
+    Checkbox: 'Checkbox',//勾选
+    Color: 'Color',//颜色选择
+    Icon: 'Icon',//图标选择
+    Date:'Date',//日期
+    Time:'Time',//时间
+    Refer:'Refer',//参照
+    SelectTree:'SelectTree',//下拉🌲
+    Textarea:'Textarea',//多行文本
     TitleGroup:'TitleGroup',//表头合并配置
     EventActions:'EventActions',//组织事件动作配置
     MathFormula:'MathFormula',//计算公式列配置
     LogicFormula:'LogicFormula',//逻辑公式配置
-    EditorIcons:'EditorIcons',
+    EditorIcons:'EditorIcons',//仅限富编辑器使用的图标
     Pagination: 'Pagination',//分页配置
     SwitchText:'SwitchText',
-    EnumSelect:'EnumSelect',
+    EnumSelect:'EnumSelect',//枚举档案
     ReferEditRelation:'ReferEditRelation', // 参照编辑关联项
     LocalData:'LocalData', //数据表格本地数据源配置
-    RowMenu: 'RowMenu',
-    FilterAreaConfig: 'FilterAreaConfig',
-    EChartData: 'EChartData'
+    RowMenu: 'RowMenu',//表格行操作菜单
+    FilterAreaConfig: 'FilterAreaConfig',//筛选栏配置
+    EChartData: 'EChartData'//Echart图表配置
 };
